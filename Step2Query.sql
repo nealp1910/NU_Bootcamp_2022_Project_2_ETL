66034001eeeb257933771ba61544eb12cf66148c
Alfredo Garcia <und3adgam3r@live.com>
2022-05-26 20:18:04 -0500

    Finished SQLs

---
+++ Step2Query.sql
@@ -0,0 +1,23 @@
+SELECT b."Name", d."FK Accuracy", b."Age",b."Overall",b."Club",b."Value"
+FROM "Basic_info" b
+JOIN "Detailed_info" d
+ON (b."ID" = d."ID")
+ORDER BY d."FK Accuracy" DESC;
+
+SELECT b."Name", b."Age",b."Overall",b."Club",b."Value", d."Preferred foot"
+FROM "Basic_info" b
+JOIN "Detailed_info" d
+ON (b."ID" = d."ID")
+WHERE d."Preferred foot" = 'Left'
+
+SELECT b."Name", b."Age",b."Overall",b."Club",b."Value", d."DOB"
+FROM "Basic_info" b
+JOIN "Detailed_info" d
+ON (b."ID" = d."ID")
+WHERE d."DOB" >='1992-01-01'
+AND d."DOB" <='1997-12-31'
+
+SELECT b."Name", b."Age",b."Overall",b."Club",b."Value", d."FK Accuracy", d."Ball Control", d."Shot Power" ,d."Preferred foot", d."Stamina" ,d."DOB"
+FROM "Basic_info" b
+JOIN "Detailed_info" d
+ON (b."ID" = d."ID")
